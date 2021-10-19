277ccf88bb518a9f0dc153e6eabcd54f3469cde5
g-veronika <g.veronika2021@gmail.com>
2021-10-19 20:51:02 +0200

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Navbar from '../src/components/Navbar'
 import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './pages/';
+
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Home />
     </Router>
   );
 }
