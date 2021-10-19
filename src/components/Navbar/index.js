1caa2190145f476c225bb2139236ce7ee851193c
g-veronika <g.veronika2021@gmail.com>
2021-10-19 18:14:55 +0200

    initial commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Nav, NavbarContainer, NavLogo} from './NavbarElements'
+
+
+
+function Navbar() {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo>dolla</NavLogo>    
+                </NavbarContainer>    
+            </Nav>    
+        </>
+        
+    )
+}
+
+export default Navbar
