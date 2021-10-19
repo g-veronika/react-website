a2827ad6d5cba12671f0320170621abb607a48f8
g-veronika <g.veronika2021@gmail.com>
2021-10-19 19:29:49 +0200

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from '../src/components/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
