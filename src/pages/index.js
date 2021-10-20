75f0aed0036ea9ed1a4471e27f9cfc7650242b83
g-veronika <g.veronika2021@gmail.com>
2021-10-20 09:08:52 +0200

    add homepage video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import HeroSection from '../components/HeroSection';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
@@ -14,6 +15,7 @@ const Home = () => {
         <>
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Navbar toggle={toggle} />
+            <HeroSection />
         </>
     )
 }
