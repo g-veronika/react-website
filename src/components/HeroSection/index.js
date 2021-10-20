22eda4ab89040ea78393ab46b24df8bb0b9ce1e4
g-veronika <g.veronika2021@gmail.com>
2021-10-20 11:28:46 +0200

    create button element

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import { Button } from '../ButtonElement'
+import { 
+    HeroContainer, 
+    HeroBg, VideoBg, 
+    HeroContent, 
+    HeroH1, 
+    HeroP, 
+    HeroBtnWrapper, 
+    ArrowForward, 
+    ArrowRight
+} from './HeroElements'
 
 const HeroSection = () => {
+    
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    }   
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account
+                </HeroP>    
+                <HeroBtnWrapper>
+                    <Button 
+                    to='signup' 
+                    onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    primary='true'
+                    dark='true' 
+                    >
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
