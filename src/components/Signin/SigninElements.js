e0721aac15acb10620af6a49d03a175b5c454d89
g-veronika <g.veronika2021@gmail.com>
2021-10-21 12:54:57 +0200

    change background color in form

--- src/components/Signin/SigninElements.js
+++ src/components/Signin/SigninElements.js
@@ -10,12 +10,7 @@ export const Container = styled.div`
     top: 0;
     z-index:0;
     overflow: hidden;
-    background: linear-gradient(
-        108deg,
-        rgba(1, 147, 86, 1) 0%
-        rgba(10, 201, 122, 1) 100%
-    );
-
+    background: rgba(1, 147, 86, 1);
 `
 
 export const FormWrap = styled.div`
